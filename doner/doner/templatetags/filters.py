45ebc559fd4d37c64f7d2c4429d0a6091d3cbda6
Tomasz Karbownicki <tomasz@karbownicki.com>
2014-08-27 22:48:57 +0200

    Bigger forms

---
+++ doner/doner/templatetags/filters.py
@@ -0,0 +1,22 @@
+from django import template
+register = template.Library()
+
+
+@register.filter(name='add_attributes')
+def add_attributes(field, css):
+    attrs = {}
+    definition = css.split(',')
+
+    for d in definition:
+        if ':' not in d:
+            attrs['class'] = d
+        else:
+            t, v = d.split(':')
+            attrs[t] = v
+
+    return field.as_widget(attrs=attrs)
+
+
+@register.filter('fieldtype')
+def fieltype(field):
+    return field.field.widget.__class__.__name__
